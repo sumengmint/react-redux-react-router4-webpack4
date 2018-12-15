9a84af864783a2545b14f9627e0756a1c355e660
苏萌 <sumeng06606@hellobike.com>
2018-12-15 17:40:08 +0800

    git test

--- mock/data.js
+++ mock/data.js
@@ -7,7 +7,8 @@ router.all('/data', (req, res, next) => {
     var data = Mock.mock({
         resultCode: 200,
         resultJson: {
-            'id|10000-20000': 10000,//id: 10000-20000之间的随机一个数字
+            'id|10000-20000': 12000,//id: 10000-20000之间的随机一个数字
+
             'name': '@cname',//name: 随机生成一个中文名
             'star|0-5': '☆',//star: 指定的字符串重复0-5次生成
             'image': '',//image:
